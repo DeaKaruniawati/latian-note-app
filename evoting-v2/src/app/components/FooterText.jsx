8b9f6b8314051e6d46934ac3ea2e5de03c03f0c3
DeaKaruniawati <163228785+DeaKaruniawati@users.noreply.github.com>
2024-06-03 12:43:20 +0700

    Delete evoting-v2 directory

--- evoting-v2/src/app/components/FooterText.jsx
+++
@@ -1,5 +0,0 @@
-const FooterText = () => {
-  return <span className='footer-text font-light'><b className='text-primary font-bold'>evoting</b> by pplgskansaka.com</span>
-}
-
-export default FooterText
--- evoting-v2/src/app/consts/strings.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = 'https://pilketosapi.smkn1kandeman.sch.id/'
-// export const API_URL = 'http://localhost:5000'
